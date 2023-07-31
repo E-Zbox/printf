af13ff5ab0b8a2a154f69d91e929a31994802619
JUDE UCHECHUKWU OBIEKEZIE <abbot_jude@outlook.com>
2023-07-31 14:49:47 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -33,8 +33,8 @@ i++;
 else if (format[i +1] != '\0')
 {
 getFunc = get_func(format[i +1]);
-count += (getFunc ? getFunc(args_list) : _putchar(format[i]) + 
-_putchar(format[i +1]));
+count += (getFunc ? getFunc(args_list) :
+_putchar(format[i]) + _putchar(format[i +1]));
 i++;
 }
 }
